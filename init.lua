dfaebe4ad1065faef218c613a5a16f2bb445fb5b
zmv7 <72821250+zmv7@users.noreply.github.com>
2022-02-23 22:13:19 +0500

    multicraft2 compability

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 local HUD = nil
 minetest.register_on_punchnode(function(pos, node)
-if core.localplayer:get_control().aux1 and tostring(core.localplayer:get_velocity()) == '(0, 0, 0)'  then
+if core.localplayer:get_control().aux1 and core.pos_to_string(core.localplayer:get_velocity()) == '(0,0,0)' then
  if not HUD then
     HUD = core.localplayer:hud_add({
       hud_elem_type = "waypoint",
