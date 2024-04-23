a0e3d8f54ef3c1c0b0e88b2f6e66c6d9a21b222c
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2024-04-23 18:52:52 +0500

    Minor redefinements

--- init.lua
+++ init.lua
@@ -1,16 +1,17 @@
-local HUD = nil
-minetest.register_on_punchnode(function(pos, node)
-if core.localplayer:get_control().aux1 and core.pos_to_string(core.localplayer:get_velocity()) == '(0,0,0)' then
- if not HUD then
-    HUD = core.localplayer:hud_add({
-      hud_elem_type = "waypoint",
-      name = "Distance:",
-      number = 0xFF0000,
-      world_pos = pos})
-    if not HUD then return end
- else
-core.localplayer:hud_remove(HUD)
-HUD = nil
- end
-end
+local WP = nil
+core.register_on_punchnode(function(pos, node)
+	local ctrl = core.localplayer:get_control()
+	if ctrl.aux1 and ctrl.sneak then
+		if not WP then
+			WP = core.localplayer:hud_add({
+				hud_elem_type = "waypoint",
+				name = core.pos_to_string(pos),
+				number = 0xFF0000,
+				world_pos = pos})
+			if not WP then return end
+		else
+			core.localplayer:hud_remove(WP)
+			WP = nil
+		end
+	end
 end)
